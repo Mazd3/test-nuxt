bc58c1b7025984d98080c40a4c4ca5e26680b9e6
Mazd3 <haziullin2000@gmail.com>
2026-02-17 01:02:00 +0500

    chore(nuxt): add runtime config for API

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/styles/index.scss'],
+  
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: 'https://test-task-api.tapir.ws',
+    }
+  },
+  
   components: [
     {
       path: '~/components',
