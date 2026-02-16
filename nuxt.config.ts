d0abea6449329f1ae25523ae6cc260a0949dba70
Mazd3 <haziullin2000@gmail.com>
2026-02-16 17:44:49 +0500

    Init

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['~/assets/styles/index.scss'],
+  modules: [
+    [
+      '@nuxt/fonts',
+      {
+        families: [
+          {
+            name: 'Golos Text',
+            provider: 'google',
+            weights: [400, 500, 600, 700]
+          }
+        ]
+      }
+    ]
+  ]
+})
