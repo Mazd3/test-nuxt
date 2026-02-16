bbf7bab360f5b5668a953b193c07507c0607b9d0
Mazd3 <haziullin2000@gmail.com>
2026-02-16 19:54:37 +0500

    feat(ui): add container

---
+++ app/components/ui/container/Container.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+type ContainerTag = 'div' | 'section' | 'main' | 'header' | 'footer' | 'nav' | 'article' | 'aside';
+
+const props = withDefaults(
+	defineProps<{
+		as?: ContainerTag;
+	}>(),
+	{
+		as: 'div',
+	},
+);
+
+</script>
+
+<template>
+	<component :is="props.as" class="container">
+		<slot />
+	</component>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    max-width: var(--max-width);
+    margin: 0 auto;
+    padding: 0 var(--container-padding);
+
+    @media (max-width: 768px) {
+        padding: 0 var(--container-padding-mobile);
+    }
+}
+</style>
