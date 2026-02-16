7d2b72b550239e98986b657fb7837d87569456e6
Mazd3 <haziullin2000@gmail.com>
2026-02-17 02:47:00 +0500

    feat(layouts): add default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Header } from '~/components/header';
+import { Footer } from '~/components/footer';
+</script>
+
+<template>
+    <Header />
+    <main>
+        <slot />
+    </main>
+    <Footer />
+</template>
