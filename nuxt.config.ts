43ced0cc3110e9db941c311f90ea24c7928c14dd
Mazd3 <haziullin2000@gmail.com>
2026-02-16 18:57:30 +0500

    chore(nuxt): disable auto-imports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/styles/index.scss'],
+  imports: {
+    autoImport: false,
+  },
   modules: [
     [
       '@nuxt/fonts',
