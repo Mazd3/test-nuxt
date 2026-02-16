5605d44c7732429b11f80f518107448aa4a1211f
Mazd3 <haziullin2000@gmail.com>
2026-02-16 19:56:16 +0500

    chore(nuxt): disable registration ts files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/styles/index.scss'],
+  components: [
+    {
+      path: '~/components',
+      extensions: ['vue']
+    }
+  ],
   imports: {
     autoImport: false,
   },
