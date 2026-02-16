10084d34897e1cde720759c52774eaf9b4ef8c87
Mazd3 <haziullin2000@gmail.com>
2026-02-16 19:57:07 +0500

    feat(ui): add square variant for button

--- app/components/ui/button/Button.vue
+++ app/components/ui/button/Button.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
 const props = withDefaults(defineProps<{
-    variant?: 'primary' | 'outline';
+    variant?: 'primary' | 'outline' | 'ghost';
+    square?: boolean;
 }>(), {
     variant: 'primary',
+    square: false,
 });
 </script>
 
 <template>
-    <button class="button" :class="`button--${props.variant}`">
+    <button 
+        class="button" 
+        :class="[
+            `button--${props.variant}`,
+            { 'button--square': props.square }
+        ]"
+    >
         <slot />
     </button>
 </template>
@@ -24,14 +32,30 @@ const props = withDefaults(defineProps<{
     font-weight: 400;
     cursor: pointer;
 
+    // VARIANTS
+
     &--primary {
         background-color: var(--color-red);
         color: var(--color-white);
     }
+
     &--outline {
         background-color: var(--color-white);
         color: var(--color-black);
         border: 1px solid var(--color-black);
+    }
+
+    &--ghost {
+        background-color: var(--color-grey);
+        color: var(--color-black);
+        border: none;
+    }
+
+    // SQUARE
+
+    &--square {
+        padding: 0;
+        aspect-ratio: 1/1;
     }
 }
 </style>
